fdff6d62e7a65dd6f47c8cd47d4bd6543ab2d07d
nidhoggfgg <nidhoggfgg@gmail.com>
2022-05-28 15:46:28 +0800

    add algorithms/merge.rs

---
+++ algorithms/algorithms_rust/src/sorting/merge.rs
@@ -0,0 +1,43 @@
+pub fn merge_sort<T: PartialOrd + Copy>(array: &mut [T]) {
+    if array.len() > 1 {
+        let mid = array.len() / 2;
+        merge_sort(&mut array[..mid]);
+        merge_sort(&mut array[mid..]);
+        merge(array, mid);
+    }
+}
+
+fn merge<T: PartialOrd + Copy>(array: &mut [T], mid: usize) {
+    let aux = array.to_vec();
+    let mut l = 0;
+    let mut r = mid;
+
+    for v in array {
+        if r >= aux.len() || (l < mid && aux[l] < aux[r]) {
+            *v = aux[l];
+            l += 1;
+        } else {
+            *v = aux[r];
+            r += 1;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rand::prelude::*;
+    use super::*;
+
+    #[test]
+    fn random() {
+        let mut rng = thread_rng();
+        let mut random_vec: Vec<u32> = (1..100).collect();
+        random_vec.shuffle(&mut rng);
+        let mut random_vec_copy = random_vec.clone();
+
+        merge_sort(&mut random_vec);
+        random_vec_copy.sort();
+
+        assert_eq!(random_vec, random_vec_copy);
+    }
+}
--- algorithms/algorithms_rust/src/sorting/mod.rs
+++ algorithms/algorithms_rust/src/sorting/mod.rs
@@ -1,2 +1,3 @@
 pub mod insertion;
 pub mod selection;
+pub mod merge;
