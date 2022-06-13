e0d7b62ae1c641ea1b3c6faef19cf6d13ac87735
nidhoggfgg <nidhoggfgg@gmail.com>
2022-06-13 18:08:08 +0800

    add cli interface

--- passwd_generator/rust/src/main.rs
+++ passwd_generator/rust/src/main.rs
@@ -1,9 +1,50 @@
+use clap::Parser;
+
 use generator::gen_passwd;
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(long = "auth", help = "the target for generate passwd")]
+    auth: String,
+
+    #[clap(
+        long = "target",
+        help = "the unique string, pls remember it and don't tell anybody!"
+    )]
+    target: String,
+
+    #[clap(
+        short = 'd',
+        default_value_t = 16,
+        help = "the number of digits of the passwd"
+    )]
+    digits: u32,
+
+    #[clap(short = 'u', help = "add uppercase char to passwd [default: false]")]
+    uppercase: bool,
+
+    #[clap(short = 'n', help = "add number to passwd [default: false]")]
+    number: bool,
+
+    #[clap(
+        short = 's',
+        default_value = ".@_-:!",
+        help = "the symbols used in passwd"
+    )]
+    symbols: String,
+}
+
 fn main() {
-    let auth = "ᑋᵉᑊᑊᵒ ᵕ̈ ᑋᵉᑊᑊᵒ";
-    let target = "some";
-    let symbols = ['.', '@', '_', '-', ':', '!'];
-    let passwd = gen_passwd(auth, target, 20, true, true, &symbols);
-    println!("passwd: {}", passwd);
+    let args = Cli::parse();
+    let symbols: Vec<char> = args.symbols.chars().collect();
+    let passwd = gen_passwd(
+        &args.auth,
+        &args.target,
+        args.digits,
+        args.uppercase,
+        args.number,
+        &symbols,
+    );
+    println!("{}", passwd);
 }
