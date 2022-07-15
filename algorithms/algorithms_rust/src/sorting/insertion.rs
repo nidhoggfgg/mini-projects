9bc7d087db9231f307dde127d8013314afe73852
nidhoggfgg <nidhoggfgg@gmail.com>
2022-07-15 21:18:37 +0800

    fix algorithm bug and add test

--- algorithms/algorithms_rust/src/sorting/insertion.rs
+++ algorithms/algorithms_rust/src/sorting/insertion.rs
@@ -1,6 +1,6 @@
 pub fn insertion_sort<T: PartialOrd>(array: &mut [T]) {
     for i in 1..array.len() {
-        for j in (1..i).rev() {
+        for j in (1..=i).rev() {
             if array[j] > array[j - 1] {
                 break;
             }
@@ -28,10 +28,23 @@ mod tests {
     }
 
     #[test]
-    fn normal() {
-        // this is not good, just for fun :)
+    fn double() {
         let mut array = vec![4.5, 1.2, 3.4, 5.6];
         insertion_sort(&mut array);
         assert_eq!(array, vec![1.2, 3.4, 4.5, 5.6]);
+    }
+
+    #[test]
+    fn integer() {
+        let mut array = vec![1, 4, 5, 9, 2, 6];
+        insertion_sort(&mut array);
+        assert_eq!(array, vec![1, 2, 4, 5, 6, 9]);
+    }
+
+    #[test]
+    fn range() {
+        let mut array: Vec<u32> = (0..1000).rev().collect();
+        insertion_sort(&mut array);
+        assert_eq!(array, (0..1000).collect::<Vec<u32>>());
     }
 }
