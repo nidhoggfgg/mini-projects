2cba53ef2d95b1bca68c76f4e82a51a1683b835c
nidhoggfgg <nidhoggfgg@gmail.com>
2022-12-13 23:57:24 +0800

    drawille-rust

--- drawille/drawille-rust/src/lib.rs
+++ drawille/drawille-rust/src/lib.rs
@@ -1,3 +1,5 @@
+use std::cmp;
+
 #[rustfmt::skip]
 const PIXEL_MAP: [[u32; 2]; 4] = [[0x01, 0x08],
                                   [0x02, 0x10],
@@ -52,6 +54,29 @@ impl Canvas {
         }
     }
 
+    pub fn line(&mut self, x1: f64, y1: f64, x2: f64, y2: f64) {
+        let (x1, y1) = (normalize(x1), normalize(y1));
+        let (x2, y2) = (normalize(x2), normalize(y2));
+        let d = |v1, v2| if v1 <= v2 {
+            (v2 - v1, 1.0)
+        } else {
+            (v1 - v2, -1.0)
+        };
+
+        let (xdiff, xdir) = d(x1, x2);
+        let (ydiff, ydif) = d(y1, y2);
+        let r = cmp::max(xdiff, ydiff);
+
+        for i in 0..=r {
+            let r = r as f64;
+            let i = i as f64;
+            let (xd, yd) = (xdiff as f64, ydiff as f64);
+            let x = x1 as f64 + i * xd / r * xdir;
+            let y = y1 as f64 + i * yd / r * ydif;
+            self.set(x, y);
+        }
+    }
+
     fn unset(&mut self, x: f64, y: f64) {
         let (row, col) = get_pos(x, y);
         self.pad_row_col(row, col);
@@ -83,6 +108,64 @@ impl Canvas {
         let pad_num = col - self.pixels[row].len() + 1;
         let mut pad = vec![0; pad_num];
         self.pixels[row].append(&mut pad);
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Turtle {
+    pos_x: f64,
+    pos_y: f64,
+    rotation: f64,
+    brush_on: bool,
+    canvas: Canvas,
+}
+
+impl Turtle {
+    pub fn new(pos_x: f64, pos_y: f64) -> Self {
+        Self { pos_x, pos_y, rotation: 0.0, brush_on: true, canvas: Canvas::new() }
+    }
+
+    pub fn frame(&mut self) -> String {
+        self.canvas.frame()
+    }
+
+    pub fn up(&mut self) {
+        self.brush_on = false;
+    }
+
+    pub fn down(&mut self) {
+        self.brush_on = true;
+    }
+
+    pub fn forward(&mut self, step: f64) {
+        let (sr, cr) = self.rotation.to_radians().sin_cos();
+        let x = self.pos_x + cr * step;
+        let y = self.pos_y + sr * step;
+        let prev_brush = self.brush_on;
+        self.brush_on = true;
+        self.move_to(x, y);
+        self.brush_on = prev_brush;
+    }
+
+    pub fn move_to(&mut self, x: f64, y: f64) {
+        if self.brush_on {
+            self.canvas.line(self.pos_x, self.pos_y, x, y);
+        }
+
+        self.pos_x = x;
+        self.pos_y = y;
+    }
+
+    pub fn right(&mut self, angle: f64) {
+        self.rotation += angle;
+    }
+
+    pub fn left(&mut self, angle: f64) {
+        self.rotation -= angle;
+    }
+
+    pub fn back(&mut self, step: f64) {
+        self.forward(-step)
     }
 }
 
