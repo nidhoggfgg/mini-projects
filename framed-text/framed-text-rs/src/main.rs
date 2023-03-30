323bba45661456c71fb576eb81f00852308aea3a
nidhoggfgg <nidhoggfgg@gmail.com>
2023-03-30 16:11:03 +0800

    OvO

--- framed-text/framed-text-rs/src/main.rs
+++ framed-text/framed-text-rs/src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     fs::File,
-    io::{self, BufRead},
+    io::{BufRead, BufReader},
     path::Path,
     process,
 };
@@ -13,74 +13,45 @@ fn main() {
         process::exit(2);
     }
 
-    if let Ok(lines) = read_lines(&args[1]) {
-        let max_width = get_max_width(&lines);
-        add_head(max_width, ('┌', '┐', '─', '│'));
-        add_frame(&lines, max_width, ('├', '┤', '└', '┘', '─', '│'));
-    }
+    let lines = read_lines(&args[1]);
+    let max_width = get_max_width(&lines);
+    add_head(max_width, ('┌', '┐', '─', '│'), "- □ x");
+    add_body(&lines, max_width, ('├', '┤', '─', '│'));
+    add_tail(max_width, ('└', '┘', '─'))
 }
 
 fn get_max_width(lines: &[String]) -> usize {
-    let mut max_width: usize = 0;
-    for line in lines {
-        if line.len() > max_width {
-            max_width = line.len();
-        }
-    }
+    let max_width = lines.iter().map(|s| s.len()).max().unwrap_or(0);
     max_width + 3
 }
 
-fn add_head(max_width: usize, frame_char: (char, char, char, char)) {
+fn add_head(max_width: usize, frame_char: (char, char, char, char), icons: &str) {
     let (lt, rt, h, v) = frame_char;
-
-    let mut _h = String::new();
-    _h.push(h);
-
-    println!("{}{}{}", lt, _h.repeat(max_width + 2), rt);
-    println!("{0}{1:>width$}{0}", v, "- □ x ", width = max_width + 2);
+    let h = format!("{h}");
+    println!("{lt}{}{rt}", h.repeat(max_width + 2));
+    println!("{v} {icons:>width$} {v}", width = max_width);
 }
 
-fn add_frame(
-    lines: &[String],
-    max_width: usize,
-    frame_char: (char, char, char, char, char, char),
-) {
-    let (lm, rm, lb, rb, h, v) = frame_char;
-
-    let mut _h = String::new();
-    _h.push(h);
-
-    println!("{}{}{}", lm, _h.repeat(max_width + 2), rm);
-
+fn add_body(lines: &[String], max_width: usize, frame_char: (char, char, char, char)) {
+    let (lm, rm, h, v) = frame_char;
+    let h = format!("{h}");
+    println!("{lm}{}{rm}", h.repeat(max_width + 2));
     for line in lines {
-        println!("{0} {1:width$} {0}", v, line, width = max_width);
+        println!("{v} {line:width$} {v}", width = max_width);
     }
-
-    println!(
-        "{}{:width$}{}",
-        lb,
-        _h.repeat(max_width + 2),
-        rb,
-        width = max_width
-    );
 }
 
-fn read_lines<P>(filename: &P) -> io::Result<Vec<String>>
+fn add_tail(max_width: usize, frame_char: (char, char, char)) {
+    let (lb, rb, h) = frame_char;
+    let h = format!("{h}");
+    println!("{lb}{}{rb}", h.repeat(max_width + 2));
+}
+
+fn read_lines<P>(filename: &P) -> Vec<String>
 where
     P: AsRef<Path>,
 {
-    let mut lines: Vec<String> = Vec::new();
-    let _lines = _read_lines(filename).unwrap();
-    for line in _lines.flatten() {
-        lines.push(line);
-    }
-    Ok(lines)
-}
-
-fn _read_lines<P>(filename: &P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+    let file = File::open(filename).expect("can't open file");
+    let buf = BufReader::new(file);
+    buf.lines().map(|l| l.expect("can't parse line")).collect()
 }
